690b1d39b6b6c27e3480f5061c748d19a440ebae
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-05-15 21:57:56 +0200

    Add github actions

--- src/test/java/de/michaelafleig/serefactoring/RentalTest.java
+++ src/test/java/de/michaelafleig/serefactoring/RentalTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class RentalTest {
 
     @Test
-    void getAmount1() {
+    void statement1() {
         Rental r = new Rental(new Movie("Test", Movie.NEW_RELEASE), 5);
         Customer c = new Customer("Er");
         c.addRental(r);
@@ -19,32 +19,32 @@ public class RentalTest {
     }
 
     @Test
-    void getAmount2() {
+    void statement2() {
         Rental r = new Rental(new Movie("Test", Movie.CHILDRENS), 5);
-        //assertEquals(4.5, r.getAmount());
+        Rental r2 = new Rental(new Movie("Test", Movie.REGULAR), 10);
+        Customer c = new Customer("Er");
+        c.addRental(r);
+        c.addRental(r2);
+        assertEquals("Rental Record for Er\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tTest\t\t5\t4.5\n" +
+                "\tTest\t\t10\t14.0\n" +
+                "Amount owed is 18.5\n" +
+                "You earned 2 frequent renter points", c.statement());
     }
 
     @Test
-    void getAmount3() {
-        Rental r = new Rental(new Movie("Test", Movie.REGULAR), 5);
-        //assertEquals(6.5, r.getAmount());
-    }
-
-    @Test
-    void getAmount4() {
-        Rental r = new Rental(new Movie("Test", Movie.NEW_RELEASE), 1);
-        //assertEquals(3, r.getAmount());
-    }
-
-    @Test
-    void getAmount5() {
+    void statement3() {
         Rental r = new Rental(new Movie("Test", Movie.CHILDRENS), 1);
-        //assertEquals(1.5, r.getAmount());
-    }
-
-    @Test
-    void getAmount6() {
-        Rental r = new Rental(new Movie("Test", Movie.REGULAR), 1);
-        //assertEquals(2, r.getAmount());
+        Rental r2 = new Rental(new Movie("Test", Movie.REGULAR), 2);
+        Customer c = new Customer("Er");
+        c.addRental(r);
+        c.addRental(r2);
+        assertEquals("Rental Record for Er\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tTest\t\t1\t1.5\n" +
+                "\tTest\t\t2\t2.0\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 2 frequent renter points", c.statement());
     }
 }
