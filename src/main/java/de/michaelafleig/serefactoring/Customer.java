fa29d08e09899c300e437797939bbde5a86f9744
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-05-16 16:50:37 +0200

    Removed string concatenation
    due to build problems

--- src/main/java/de/michaelafleig/serefactoring/Customer.java
+++ src/main/java/de/michaelafleig/serefactoring/Customer.java
@@ -30,7 +30,7 @@ class Customer {
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
-        result.concat("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
+        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
@@ -43,12 +43,12 @@ class Customer {
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            result.concat("\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n");
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
-        result.concat("Amount owed is " + String.valueOf(totalAmount) + "\n");
-        result.concat("You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points");
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
 
