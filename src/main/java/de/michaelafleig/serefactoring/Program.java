5f8ecbbfb2342714857ee0022e856e35b22f696d
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-05-16 17:51:21 +0200

    Rename Variable
    https://refactoring.com/catalog/renameVariable.html

--- src/main/java/de/michaelafleig/serefactoring/Program.java
+++ src/main/java/de/michaelafleig/serefactoring/Program.java
@@ -21,15 +21,15 @@ public class Program {
     public static void main(String args[]) {
         String result;
         System.out.println("Welcome to the de.michaelafleig.serefactoring.Movie Store");
-        Movie m1 = new Movie(MOVIE_TITLE_1, Movie.NEW_RELEASE);
-        Movie m2 = new Movie(MOVIE_TITLE_2, Movie.CHILDREN);
-        Rental r1 = new Rental(m1, DAYS_RENTED_10);
-        Rental r2 = new Rental(m2, DAYS_RENTED_5);
-        Customer c1 = new Customer(CUSTOMER_NAME_1);
-        c1.addRental(r1);
-        c1.addRental(r2);
+        Movie movie1   = new Movie(MOVIE_TITLE_1, Movie.NEW_RELEASE);
+        Movie movie2   = new Movie(MOVIE_TITLE_2, Movie.CHILDREN);
+        Rental rental1 = new Rental(movie1, DAYS_RENTED_10);
+        Rental rental2 = new Rental(movie2, DAYS_RENTED_5);
+        Customer customer1 = new Customer(CUSTOMER_NAME_1);
+        customer1.addRental(rental1);
+        customer1.addRental(rental2);
         System.out.println("Let's get the Statement");
-        result = c1.statement();
+        result = customer1.statement();
         System.out.println(result);
     }
 }
--- src/test/java/de/michaelafleig/serefactoring/RentalTest.java
+++ src/test/java/de/michaelafleig/serefactoring/RentalTest.java
@@ -15,44 +15,44 @@ public class RentalTest {
 
     @Test
     void statement1() {
-        Rental r = new Rental(new Movie(NEW_TITLE_1, Movie.NEW_RELEASE), NEW_DAYS_RENTED_5);
-        Customer c = new Customer(NEW_NAME_1);
-        c.addRental(r);
+        Rental rental = new Rental(new Movie(NEW_TITLE_1, Movie.NEW_RELEASE), NEW_DAYS_RENTED_5);
+        Customer customer = new Customer(NEW_NAME_1);
+        customer.addRental(rental);
         assertEquals("Rental Record for Er\n" +
                 "\tTitle\t\tDays\tAmount\n" +
                 "\tTest\t\t5\t15.0\n" +
                 "Amount owed is 15.0\n" +
-                "You earned 2 frequent renter points", c.statement());
+                "You earned 2 frequent renter points", customer.statement());
     }
 
 
     @Test
     void statement2() {
-        Rental r = new Rental(new Movie(NEW_TITLE_1 Movie.CHILDREN), NEW_DAYS_RENTED_5);
-        Rental r2 = new Rental(new Movie(NEW_TITLE_1, Movie.REGULAR), NEW_DAYS_RENTED_10);
-        Customer c = new Customer(NEW_NAME_1;
-        c.addRental(r);
-        c.addRental(r2);
+        Rental rental1 = new Rental(new Movie(NEW_TITLE_1, Movie.CHILDREN), NEW_DAYS_RENTED_5);
+        Rental rental2 = new Rental(new Movie(NEW_TITLE_1, Movie.REGULAR), NEW_DAYS_RENTED_10);
+        Customer customer = new Customer(NEW_NAME_1);
+        customer.addRental(rental1);
+        customer.addRental(rental2);
         assertEquals("Rental Record for Er\n" +
                 "\tTitle\t\tDays\tAmount\n" +
                 "\tTest\t\t5\t4.5\n" +
                 "\tTest\t\t10\t14.0\n" +
                 "Amount owed is 18.5\n" +
-                "You earned 2 frequent renter points", c.statement());
+                "You earned 2 frequent renter points", customer.statement());
     }
 
     @Test
     void statement3() {
-        Rental r = new Rental(new Movie(NEW_TITLE_1, Movie.CHILDREN), NEW_DAYS_RENTED_1);
-        Rental r2 = new Rental(new Movie(NEW_TITLE_1, Movie.REGULAR), NEW_DAYS_RENTED_2);
-        Customer c = new Customer(NEW_NAME_1);
-        c.addRental(r);
-        c.addRental(r2);
+        Rental rental1 = new Rental(new Movie(NEW_TITLE_1, Movie.CHILDREN), NEW_DAYS_RENTED_1);
+        Rental rental2 = new Rental(new Movie(NEW_TITLE_1, Movie.REGULAR), NEW_DAYS_RENTED_2);
+        Customer customer = new Customer(NEW_NAME_1);
+        customer.addRental(rental1);
+        customer.addRental(rental2);
         assertEquals("Rental Record for Er\n" +
                 "\tTitle\t\tDays\tAmount\n" +
                 "\tTest\t\t1\t1.5\n" +
                 "\tTest\t\t2\t2.0\n" +
                 "Amount owed is 3.5\n" +
-                "You earned 2 frequent renter points", c.statement());
+                "You earned 2 frequent renter points", customer.statement());
     }
 }
